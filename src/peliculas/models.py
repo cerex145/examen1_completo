7212eecfb5fe6f950d2941aa60ede88724b67780
Carlos Valeriano <carlos.valeriano@tecsup.edu.pe>
2025-09-10 16:26:18 -0500

    1er

--- src/peliculas/models.py
+++ src/peliculas/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    nacionalidad = models.CharField(max_length=100)
 
-# Create your models here.
+    def __str__(self):
+        return self.nombre
+
+class Peliculas(models.Model):
+    titulo = models.CharField(max_length=100, verbose_name="Título")
+    estreno = models.DateField()
+    genero = models.CharField(max_length=100)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, related_name='peliculas')
+
+    def __str__(self):
+        return self.titulo
+
